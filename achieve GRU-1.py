8b7b424db0d8ed4410a7ebd689bf581bbaaf3313
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-09-20 09:17:02 +0800

    Add files via upload

---
+++ achieve GRU-1.py
@@ -0,0 +1,96 @@
+import time
+import math
+import numpy as np
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import zipfile
+
+import sys
+sys.path.append('F:/anaconda3/Lib/site-packages')
+import d2lzh_pytorch as d2l
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def load_data_jay_lyrics():
+    """加载周杰伦歌词数据集"""
+    with zipfile.ZipFile('./data/jaychou_lyrics.txt.zip') as zin:
+        with zin.open('jaychou_lyrics.txt') as f:
+            corpus_chars = f.read().decode('utf-8')
+    corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
+    corpus_chars = corpus_chars[0:10000]
+    idx_to_char = list(set(corpus_chars))   # 单词的列表
+    char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])   # 单词和数字组合的字典
+    vocab_size = len(char_to_idx)
+    corpus_indices = [char_to_idx[char] for char in corpus_chars]    # 将歌词的单词转换为数字（由上面的字典定义的）
+    return corpus_indices, char_to_idx, idx_to_char, vocab_size
+
+(corpus_indices, char_to_idx, idx_to_char, vocab_size) = load_data_jay_lyrics()
+
+'''
+GRU从零开始实现
+'''
+
+'''
+初始化模型参数:
+下面的代码对模型参数进行初始化。超参数num_hiddens定义了隐藏单元的个数。
+'''
+num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+print('will use', device)
+
+def get_params():
+    def _one(shape):
+        ts = torch.tensor(np.random.normal(0, 0.01, size=shape), device=device, dtype=torch.float32)
+        return torch.nn.Parameter(ts, requires_grad=True)
+    def _three():
+        return (_one((num_inputs, num_hiddens)),
+                _one((num_hiddens, num_hiddens)),
+                torch.nn.Parameter(torch.zeros(num_hiddens, device=device, dtype=torch.float32), requires_grad=True))  # 广播机制可以相加
+
+    W_xz, W_hz, b_z = _three()  # 更新门参数
+    W_xr, W_hr, b_r = _three()  # 重置门参数
+    W_xh, W_hh, b_h = _three()  # 候选隐藏状态参数
+
+    # 输出层参数
+    W_hq = _one((num_hiddens, num_outputs))
+    b_q = torch.nn.Parameter(torch.zeros(num_outputs, device=device, dtype=torch.float32), requires_grad=True)
+    return nn.ParameterList([W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q])
+
+'''
+定义模型:
+下面的代码定义隐藏状态初始化函数init_gru_state。同6.4节（循环神经网络的从零开始实现）中定义的init_rnn_state函数一样，
+它返回由一个形状为(批量大小, 隐藏单元个数)的值为0的Tensor组成的元组。
+'''
+def init_gru_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device), )
+
+'''
+下面根据门控循环单元的计算表达式定义模型。
+'''
+def gru(inputs, state, params):
+    W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state
+    outputs = []
+    for X in inputs:
+        Z = torch.sigmoid(torch.matmul(X, W_xz) + torch.matmul(H, W_hz) + b_z)
+        R = torch.sigmoid(torch.matmul(X, W_xr) + torch.matmul(H, W_hr) + b_r)
+        H_tilda = torch.tanh(torch.matmul(X, W_xh) + torch.matmul(R * H, W_hh) + b_h)
+        H = Z * H + (1 - Z) * H_tilda
+        Y = torch.matmul(H, W_hq) + b_q
+        outputs.append(Y)
+    return outputs, (H,)
+
+'''
+训练模型并创作歌词:
+我们在训练模型时只使用相邻采样。设置好超参数后，我们将训练模型并根据前缀“分开”和“不分开”分别创作长度为50个字符的一段歌词。
+'''
+num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
+pred_period, pred_len, prefixes = 40, 50, ['分开', '不分开']
+
+'''
+我们每过40个迭代周期便根据当前训练的模型创作一段歌词。
+'''
+d2l.train_and_predict_rnn(gru, get_params, init_gru_state, num_hiddens,
+                          vocab_size, device, corpus_indices, idx_to_char,
+                          char_to_idx, False, num_epochs, num_steps, lr,
+                          clipping_theta, batch_size, pred_period, pred_len,
+                          prefixes)
